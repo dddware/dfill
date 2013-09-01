0375927bd2b5131c3aa84964219726361d334ca3
neemzy <tom.panier@free.fr>
2013-09-01 14:30:31 +0200

    optimized js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -9,13 +9,19 @@
 
             $.getJSON('/api/' + amount, function(json) {
                 app.resultBlock.html(json.join(' '));
-                autoResizeTextareas();
-                $('.result').select();
+                app.resultBlock.css('height', '0px');
+                app.resultBlock.css('height', ($that[0].scrollHeight + parseInt($that.css('padding-top')) + parseInt($that.css('padding-bottom'))) + 'px');
+                app.resultBlock.select();
             });
         }
     };
 
     App.amountSelect.on('change', function() {
+        App.resultBlock.css({
+            'min-height': $that.css('height'),
+            overflow: 'hidden'
+        });
+
         App.loadLines($(this).val());
     }).change();
 })
